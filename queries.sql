2fc0a5d71216de1d77c1720d460c5580f319fe05
Alyssa Denary <alyssa.denary@gmail.com>
2023-03-14 15:38:06 -0400

    add car owners w/2+ cars, avg price 10K+

--- queries.sql
+++ queries.sql
@@ -3,14 +3,21 @@
 -- Join the two tables so that every column and record appears, regardless of if
 -- there is not an owner_id.
 SELECT *
-    FROM owners
+  FROM owners
     FULL JOIN vehicles ON owners.id = vehicles.owner_id;
 
 
--- Count the number of cars for each owner who has a vehicle. Display the owners 
--- first_name and count of vehicles. The first_name should be ordered in ascending order. 
+-- Count the number of cars for each owner who has a vehicle. Display the owners
+-- first_name and count of vehicles. The first_name should be ordered in ascending order.
 SELECT o.first_name, COUNT(*)
-    FROM owners AS o 
+  FROM owners AS o
     JOIN vehicles AS v ON o.id = v.owner_id
-    GROUP BY o.first_name
-    ORDER BY o.first_name;
+  GROUP BY o.first_name
+  ORDER BY o.first_name;
+
+SELECT o.first_name, ROUND(AVG(v.price), 0) AS average_price, COUNT(*)
+  FROM owners as o
+    JOIN vehicles as v ON o.id = v.owner_id
+  GROUP BY o.first_name
+  HAVING COUNT(*) > 1 AND AVG(v.price) > 10000
+  ORDER BY o.first_name DESC;
